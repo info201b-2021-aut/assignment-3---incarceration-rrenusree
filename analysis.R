5af8c8e8773c7a3aae55483956ea2af9dddf0335
rrenusree <renusree@uw.edu>
2021-11-22 22:51:00 -0800

    assignment-3---incarceration-rrenusree

--- analysis.R
+++ analysis.R
@@ -22,35 +22,43 @@ wa_1990_incarceration_trends <- na.omit(wa_incarceration_trends[wa_incarceration
 wa_2018_incarceration_trends <- na.omit(wa_incarceration_trends[wa_incarceration_trends$year == "2018",])
 
 sum_1990_jailpop_incarceration_wa <- sum(wa_1990_incarceration_trends$total_jail_pop)
+percent_1990_jailpop_wa <- (sum_1990_jailpop_incarceration_wa/sum(wa_1990_incarceration_trends$total_pop)) *100
 sum_2018_jailpop_incarceration_wa <- sum(wa_2018_incarceration_trends$total_jail_pop)
-difference_jailpop_incarceration <- sum_2018_jailpop_incarceration_wa - sum_1990_jailpop_incarceration_wa
-percent_increase_incarceration_wa <- (difference_jailpop_incarceration/sum_1990_jailpop_incarceration_wa)*100
+percent_2018_jailpop_wa <- (sum_2018_jailpop_incarceration_wa/sum(wa_2018_incarceration_trends$total_pop)) *100
+percent_increase_incarceration_wa <- percent_2018_jailpop_wa - percent_1990_jailpop_wa
 
 
 sum_jailmales_1990_wa <- sum(wa_1990_incarceration_trends$male_adult_jail_pop)
 sum_jailmales_2018_wa <- sum(wa_2018_incarceration_trends$male_adult_jail_pop)
-difference_males <- sum_jailmales_2018_wa - sum_jailmales_1990_wa ##
+difference_males <- sum_jailmales_2018_wa - sum_jailmales_1990_wa 
 
-percent_jail_males_1990 <- (sum_jailmales_1990_wa/sum_1990_jailpop_incarceration_wa) * 100 ##
-percent_jail_males_2018 <- (sum_jailmales_2018_wa/sum_2018_jailpop_incarceration_wa) * 100 ##
+percent_jail_males_1990 <- (sum_jailmales_1990_wa/sum_1990_jailpop_incarceration_wa) * 100 
+percent_jail_males_2018 <- (sum_jailmales_2018_wa/sum_2018_jailpop_incarceration_wa) * 100 
 
-county_highest_blackjailpop <- selected_incarceration_trends[selected_incarceration_trends$black_jail_pop == max(selected_incarceration_trends$black_jail_pop), "county_name"] ##
-year_highest_blackjailpop <- selected_incarceration_trends[selected_incarceration_trends$black_jail_pop == max(selected_incarceration_trends$black_jail_pop), "year"] ##
+county_highest_blackjailpop <- selected_incarceration_trends[selected_incarceration_trends$black_jail_pop == max(selected_incarceration_trends$black_jail_pop), "county_name"] 
+year_highest_blackjailpop <- selected_incarceration_trends[selected_incarceration_trends$black_jail_pop == max(selected_incarceration_trends$black_jail_pop), "year"] 
 
 
 wa_incarceration_trends <-mutate(wa_incarceration_trends,
                                  black_jail_pop_by_totalpop = (black_jail_pop/total_jail_pop)*100)
+# percentage of black people in jail in each county
 
 wa_incarceration_trends <-mutate(wa_incarceration_trends,
                                  latinx_jail_pop_by_totalpop = (latinx_jail_pop/total_jail_pop)*100)
 
+#percentage of latino people in jail in each county
+
 wa_incarceration_trends <-mutate(wa_incarceration_trends,
                                  latinx_pop_percentage = (latinx_pop_15to64/total_pop)*100)
+
+#percentage of latino people overall in each county
                                  
 
 grouped_blackpopjail_everyyear <- wa_incarceration_trends %>%
   group_by(year) %>%
   summarize(percentage_in_jail = (sum(black_jail_pop)/sum(total_jail_pop))*100, na.rm = TRUE)
+
+#percentage of black population in jail grouped by year in Washington state 
 
 grouped_asianpopjail_everyyear <- wa_incarceration_trends %>%
   group_by(year) %>%
@@ -71,7 +79,7 @@ linechart <- ggplot(data = grouped_races, mapping = aes(x=year))+
   labs(x= "year", y = "Percentage in Jail", title = "Percentage in Jail vs Year") 
   
 
-#more calculations:
+#more calculations for summary:
 
 black_pop_1990_wa <- sum(wa_1990_incarceration_trends$black_pop_15to64)
 total_pop_1990_wa <- sum(wa_1990_incarceration_trends$total_pop)
@@ -82,24 +90,24 @@ percent_male_pop_1990_wa <- (male_pop_1990_wa/total_pop_1990_wa)*100 #
 
 black_jailpop_1990_wa <- na.omit(sum(wa_1990_incarceration_trends$black_jail_pop))
 total_jailpop_1990_wa <- na.omit(sum(wa_1990_incarceration_trends$total_jail_pop))
-percent_black_jailpop_1990_wa <- (black_jailpop_1990_wa/total_jailpop_1990_wa)*100 #
+percent_black_jailpop_1990_wa <- (black_jailpop_1990_wa/total_jailpop_1990_wa)*100 
 
 male_jailpop_1990_wa <- sum(wa_1990_incarceration_trends$male_adult_jail_pop)
-percent_male_jailpop_1990_wa <- (male_jailpop_1990_wa/total_jailpop_1990_wa)*100 #
+percent_male_jailpop_1990_wa <- (male_jailpop_1990_wa/total_jailpop_1990_wa)*100 
 
 black_pop_2018_wa <- sum(wa_2018_incarceration_trends$black_pop_15to64)
 total_pop_2018_wa <- sum(wa_2018_incarceration_trends$total_pop)
-percent_black_pop_2018_wa <- (black_pop_2018_wa/total_pop_2018_wa)*100 #
+percent_black_pop_2018_wa <- (black_pop_2018_wa/total_pop_2018_wa)*100 
 
 male_pop_2018_wa <- sum(wa_2018_incarceration_trends$male_pop_15to64)
 percent_male_pop_2018_wa <- (male_pop_2018_wa/total_pop_2018_wa)*100
 
 black_jailpop_2018_wa <- sum(wa_2018_incarceration_trends$black_jail_pop)
 total_jailpop_2018_wa <- sum(wa_2018_incarceration_trends$total_jail_pop)
-percent_black_jailpop_2018_wa <- (black_jailpop_2018_wa/total_jailpop_2018_wa)*100 #
+percent_black_jailpop_2018_wa <- (black_jailpop_2018_wa/total_jailpop_2018_wa)*100 
 
 male_jailpop_2018_wa <- sum(wa_2018_incarceration_trends$male_adult_jail_pop)
-percent_male_jailpop_2018_wa <- (male_jailpop_2018_wa/total_jailpop_2018_wa)*100 #
+percent_male_jailpop_2018_wa <- (male_jailpop_2018_wa/total_jailpop_2018_wa)*100 
 
 #scatterplot 
 
@@ -111,8 +119,8 @@ scatterplot <- ggplot(data = wa_1990_incarceration_trends) +
   
   # Add title and axis labels
   labs(
-    title = "Percentage of Latino population in jail vs Percentage of Latino population", # plot title
-    x = "Percentage of Latino population", # x-axis label
+    title = "Percentage of Latino population in jail vs Percentage of Latino population overall", # plot title
+    x = "Percentage of Latino population overall", # x-axis label
     y = "Percentage of Latino population in jail", # y-axis label
     
   )
@@ -159,6 +167,7 @@ incarceration_map <- ggplot(map_data) +
   scale_fill_continuous(limits = c(0, max(map_data$total_jail_pop)), na.value = "white", low = "yellow", high = "red") +
   blank_theme +
   ggtitle("Jail population count across Washington")
+
 
 
 
